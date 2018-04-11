1780eb9589f42283eb266a2da718db4c74c0fc7e
xiaoping <narci2008@gmail.com>
2018-04-11 15:16:53 -0700

    Basic bookmarks/ works on server

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Bookmark
 
 # Create your views here.
+
+def index(request):
+	# get all bookmarks objects
+	context = {'bookmarks': Bookmark.objects.all()}
+	return render(request, 'bookmarks/index.html', context)
