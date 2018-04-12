dd4c734fe03aee4c3ad6ef63666a744d46d3faf2
xiaoping <narci2008@gmail.com>
2018-04-12 13:04:09 -0700

    Add BookmarkForm

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from .forms import BookmarkForm
 from .models import Bookmark
 
 # Create your views here.
 
 def index(request):
-	# get all bookmarks objects
-	context = {'bookmarks': Bookmark.objects.all()}
+	if request.method == 'POST':
+		form = BookmarkForm(request.POST)
+		if form.is_valid():
+			form.save()
+	context = {'bookmarks': Bookmark.objects.all(),
+		   'form': BookmarkForm()}
 	return render(request, 'bookmarks/index.html', context)
